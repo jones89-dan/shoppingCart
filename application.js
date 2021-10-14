c4f9bc42a0cf9b659125613a80322bd70b859b5d
Daniel <jones.89.dan@gmail.com>
2021-10-13 22:48:25 -0700

    Created the add item function and the button event

--- application.js
+++ application.js
@@ -30,6 +30,34 @@ var updateTotalPrice = function () {
 
 }
 
+var addItem = function(name,cost) {
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+  $('#item-list').prepend('<div class="row item"> \
+    <div class="item-name col-xs-3"> \ ' +  name + '\
+    </div> \
+    <div class="item-price col-xs-3"> \
+      $' + cost + '.00 \
+    </div> \
+    <div class="item-qty col-xs-3"> \
+      <label>QTY</label> \
+      <input class="quantity" type="number"> \
+    </div> \
+    <div class="col-xs-1"> \
+      <button class="remove"> \
+        Remove \
+      </button> \
+    </div> \
+    <div class="item-subtotal col-xs-2"> \
+    $--.-- \
+    </div> \
+  </div>');
+}
+
+
  $(document).on('keyup', '.quantity', function(){
    updateTotalPrice();
+ });
+
+ $(document).on('click', '#add', function() {
+   addItem($('#name').val(), $('#cost').val());
  });
